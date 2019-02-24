942c5e4652a1c6be271fcfd017e76f53d75941a0
Denis Alayza <denis.alayza@gmail.com>
2019-02-24 16:32:33 -0300

    Enviando parametros por URL

--- portafolio/src/app/pages/item/item.component.ts
+++ portafolio/src/app/pages/item/item.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
+
+
 export class ItemComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe(parametros => {
+      console.log(parametros['id']);
+    });
   }
 
 }
