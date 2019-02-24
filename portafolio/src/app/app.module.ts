bcbfd936df66d4fc201649f292977aea9c81b714
Denis Alayza <denis.alayza@gmail.com>
2019-02-24 00:12:18 -0300

    Creando rutas para nuestro sitio web

--- portafolio/src/app/app.module.ts
+++ portafolio/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ItemComponent } from './pages/item/item.component';
+
 
 @NgModule({
   declarations: [
@@ -25,4 +27,6 @@ import { ItemComponent } from './pages/item/item.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
