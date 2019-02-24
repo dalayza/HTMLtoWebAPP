fe44e1577a4f888c43bbf1d191c6d3956d7a6743
Denis Alayza <denis.alayza@gmail.com>
2019-02-24 16:11:05 -0300

    Desplegando los articulos en el home

--- portafolio/src/app/services/productos.service.ts
+++ portafolio/src/app/services/productos.service.ts
@@ -11,6 +11,7 @@ import { Producto } from '../interfaces/producto.interface';
 export class ProductosService {
 
   cargando = true;
+  productos: Producto[] = [];
 
   constructor(private http: HttpClient) {
     this.cargarProductos();
@@ -20,6 +21,7 @@ export class ProductosService {
     this.http.get('https://angular-html-a76ed.firebaseio.com/productos_idx.json')
     .subscribe((resp: Producto[]) => {
       console.log(resp);
+      this.productos = resp;
       this.cargando = false;
     });
   }
